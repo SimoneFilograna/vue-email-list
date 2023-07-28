67cb3bf847b324a787d049dc6ad36670e89442c6
SimoneFilograna <simonefilogranaf12@gmail.com>
2023-07-28 14:06:40 +0200

    bonus-1

--- js/main.js
+++ js/main.js
@@ -6,21 +6,19 @@ createApp({
     data() {
         return {
             mailList: [],
-            counter: null,
         }
     },
-    
+
     methods: {},
 
     mounted() {
         for (let i = 0; i < 10; i++) {
-            this.counter++;
             axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
                 .then(axiosResp => {   
                     this.mailList.push(axiosResp.data.response)
                 })
-        }
+        }      
     }
     }
 ).mount('#app')
